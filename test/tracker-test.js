3f4d2bfa1e2997858279a04ea0575ba1c704681b
Fernando Daciuk <f.daciuk@gmail.com>
2016-08-09 17:33:03 -0300

    Improve tests

--- test/tracker-test.js
+++ test/tracker-test.js
@@ -3,24 +3,36 @@
 const test = require('tape');
 const expect = require('chai').expect;
 const tracker = require('../lib/index');
+const intercom = require('../trackers/intercom');
+const ga = require('../trackers/ga');
+
+const intercomServer = require('../trackers/server/intercom');
+const config = require('./helpers/config');
 
 test('Test tracker method', (assert) => {
 	expect(tracker).to.be.a('function');
-	expect(tracker.intercom).to.be.a('function');
-	expect(tracker.ga).to.be.a('function');
+	expect(intercom).to.be.a('function');
+	expect(ga).to.be.a('function');
+	expect(intercomServer).to.be.a('function');
 	assert.end();
 });
 
 test('Test trackers', (assert) => {
-	const intercomTracker = tracker.intercom({
-		appId: 'd3bvrtmw'
+	const intercomTracker = intercom({
+		appId: config.appId
 	});
 
-	const gaTracker = tracker.ga({ /* ga data */});
+	const intercomTrackerServer = intercomServer({
+		appId: config.appId,
+		apiKey: config.apiKey
+	});
+
+	const gaTracker = ga();
 	const eventTracker = tracker(intercomTracker, gaTracker);
 
 	console.log('\n\n\ngaTracker', gaTracker);
 	console.log('\n\n\nintercomTracker', intercomTracker);
+	console.log('\n\n\nintercomServer', intercomTrackerServer);
 	console.log('\n\n\neventTracker', eventTracker);
 
 	intercomTracker.update({
@@ -32,6 +44,13 @@ test('Test trackers', (assert) => {
 
 	eventTracker.createEvent('store_purchase', {
 		action: 'click',
+		value: 10,
+		label: 'Product Name',
+		url: 'http://www.url.com'
+	});
+
+	intercomTrackerServer.createEvent('testEvent', {
+		action: 'someAction',
 		value: 10,
 		label: 'Product Name',
 		url: 'http://www.url.com'
